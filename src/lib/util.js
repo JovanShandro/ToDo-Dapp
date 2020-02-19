a42d32c7be8360caa6ebe9fb1fe54e1039a2adbb
Jovan Shandro <shandrojovan@gmail.com>
2020-02-19 23:19:27 +0100

    Add network names in utilities.

---
+++ src/lib/util.js
@@ -0,0 +1,7 @@
+export const NETWORKS = {
+  "1": "Main Net",
+  "2": "Deprecated Morden test network",
+  "3": "Ropsten test network",
+  "4": "Rinkeby test network",
+  "42": "Kovan test network"
+};
