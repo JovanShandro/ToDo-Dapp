8b96c03ee509b2b7b71ec5b059b818f09ab944a8
Jovan Shandro <shandrojovan@gmail.com>
2020-02-20 20:54:57 +0100

    Display a banner in case metamask extension is not found.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,27 @@
 <template>
   <q-page class="flex flex-center flex-column">
-    <p>Metamask detected: {{ stats.metamask }}</p>
-    <p>Network: {{ util.NETWORKS[stats.netId] }}</p>
-    <p>Active account: {{ stats.activeAccount }}</p>
-    <p>Balance of account: {{ stats.balance }}</p>
-    <p>Todo List address: {{ contract }}</p>
+    <div v-if="stats.metamask">
+      <p>Metamask detected: {{ stats.metamask }}</p>
+      <p>Network: {{ util.NETWORKS[stats.netId] }}</p>
+      <p>Active account: {{ stats.activeAccount }}</p>
+      <p>Balance of account: {{ stats.balance }}</p>
+      <p>Todo List address: {{ listAddress }}</p>
+    </div>
+    <q-banner style="height: 200px" v-else class="bg-orange-3">
+      <div class="row q-pb-md">
+        <i
+          class="col-4 offset-4 text-center fab fa-wolf-pack-battalion fa-4x"
+        ></i>
+      </div>
+      <div class="column" style="font-size: 18px">
+        <p class="text-center">
+          Could not detect Metamask.
+        </p>
+        <p class="text-center">
+          Please make sure to connect to the Metamask extension
+        </p>
+      </div>
+    </q-banner>
   </q-page>
 </template>
 
@@ -21,7 +38,7 @@ export default {
   },
 
   computed: {
-    ...mapState("todo", ["stats", "contract"])
+    ...mapState("todo", ["stats", "listAddress", "isLoaded"])
   },
   beforeCreate() {
     this.$store.dispatch("todo/setWeb3");
