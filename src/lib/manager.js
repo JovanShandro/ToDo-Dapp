56a00dead14618e4c412f8cd807a41a076bc36ff
Jovan Shandro <shandrojovan@gmail.com>
2020-03-06 01:07:56 +0100

    Add deployed manager contracts to utils and remove deploy function.

--- src/lib/manager.js
+++ src/lib/manager.js
@@ -3,12 +3,4 @@ import compiledManager from "./interfaces/TodoListManager.json";
 const getManager = web3 => address =>
   new web3.eth.Contract(JSON.parse(compiledManager.interface), address);
 
-const deployManager = web3 => async account => {
-  const contract = await new web3.eth.Contract(
-    JSON.parse(compiledManager.interface)
-  )
-    .deploy({ data: compiledManager.bytecode })
-    .send({ from: account, gas: "1000000" });
-  return contract.options.address;
-};
-export { getManager, deployManager };
+export default getManager;
--- src/lib/util.js
+++ src/lib/util.js
@@ -1,7 +1,14 @@
 export const NETWORKS = {
-  "1": "Main Net",
-  "2": "Deprecated Morden test network",
+  // "1": "Main Net",
   "3": "Ropsten test network",
   "4": "Rinkeby test network",
+  "5": "Goerli test network",
   "42": "Kovan test network"
+};
+
+export const MANAGERS = {
+  "3": "0xf6c0ecf6bdcdb3cc50a44fa8c002bee6469a3dee",
+  "4": "0x15e772079c3e2fedf057e30258a87f8225ac11e8",
+  "5": "0xf6c0ecf6bdcdb3cc50a44fa8c002bee6469a3dee",
+  "42": "0xf6c0ecf6bdcdb3cc50a44fa8c002bee6469a3dee"
 };
